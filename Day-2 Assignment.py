d3ac734775fb1d7fc2ef09cfc6c055d9d8f47014
shitalphadtare <68630362+shitalphadtare@users.noreply.github.com>
2020-07-22 10:44:30 +0530

    Add files via upload

---
+++ Day-2 Assignment.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Arithmetic operator
+
+# In[1]:
+
+
+10**2
+
+
+# In[2]:
+
+
+5*2
+
+
+# In[3]:
+
+
+10/5
+
+
+# In[4]:
+
+
+10%3
+
+
+# In[5]:
+
+
+5+4
+
+
+# In[6]:
+
+
+10-5
+
+
+# In[8]:
+
+
+15//2
+
+
+# Comparison
+
+# In[9]:
+
+
+a=10
+b=5
+a==b
+
+
+# In[10]:
+
+
+a<b
+
+
+# In[11]:
+
+
+a>b
+
+
+# In[12]:
+
+
+a<=b
+
+
+# In[13]:
+
+
+a>=b
+
+
+# In[14]:
+
+
+a!=b
+
+
+# Assignment Operator
+
+# In[15]:
+
+
+a=5 
+b=6
+a=b
+a
+
+
+# In[16]:
+
+
+a=5
+b=6
+a+=b
+a
+
+
+# In[19]:
+
+
+a=7
+b=6
+a-=b
+a
+
+
+# In[20]:
+
+
+a=5
+b=6
+a*=b
+a
+
+
+# In[21]:
+
+
+a=10
+b=5
+a/=b
+a
+
+
+# In[22]:
+
+
+a=5
+b=2
+a**=b
+a
+
+
+# In[43]:
+
+
+a=5
+b=3
+a//=b
+
+
+# Bitwise operator(Works on bits i.e. 0/1)
+
+# In[23]:
+
+
+a=240
+b=1
+a|b
+
+
+# In[24]:
+
+
+a&b
+
+
+# Logical operator
+
+# In[34]:
+
+
+10<9 and 20>M
+
+
+# In[37]:
+
+
+10>9 or 8<9
+
+
+# In[44]:
+
+
+not(10>9 or 8<9)
+
+
+# Identity Operator
+
+# In[48]:
+
+
+a=5
+b=5
+c=a
+a is c
+
+
+# In[47]:
+
+
+a is b
+
+
+# In[49]:
+
+
+a==b
+
+
+# In[52]:
+
+
+a=5
+b=5
+c=a
+a is not c
+
+
+# In[70]:
+
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+x is not z
+
+
+# In[71]:
+
+
+x is not y
+
+
+# In[72]:
+
+
+x != y
+
+
+# Membership Operators
+
+# In[73]:
+
+
+a=["a","b","c"]
+"a" in a
+
+
+# In[74]:
+
+
+"d" in a
+
+
+# In[75]:
+
+
+"d" not in a
+
